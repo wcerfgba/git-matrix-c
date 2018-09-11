5c72c463a6d929405c3cf527afeadf142f3331aa
John Preston <wcerfgba@riseup.net>
2018-09-11 23:19:07 +0100

    Wow that was easy

--- src/main.c
+++ src/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
-int main(){
-	printf("yo");
+char *GIT_LOG_CMD = "git --no-pager log";
+
+int main() {
+	char buf[1024];
+	FILE *git = popen(GIT_LOG_CMD, "r");
+	while (fgets(buf, 1024, git)) {
+		printf(buf);
+	}
+	printf("done!\n");
 }
